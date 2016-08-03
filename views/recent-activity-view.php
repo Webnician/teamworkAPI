3624a8c21ef8ad197948410dfd786e3b5fef2793
Olaf <blaster32blaster@yahoo.com>
2016-08-02 23:00:13 -0500

    remove malarkey from view

--- views/recent-activity-view.php
+++ views/recent-activity-view.php
@@ -11,27 +11,24 @@
         $theRenderInfo = json_decode($theresponse);
 
         $theDisplay = $theRenderInfo->activity;
-  //    foreach ($theRenderInfo as $row1 => $val1) {
+
     ?><h1 style="margin-bottom: 3%;">Recent Activity</h1><?php
 
           foreach ($theDisplay as $row => $value) { ?>
 
               <h2 style="margin-bottom: 3%;"><?php echo $row;?></h2>
               <?php
-              //echo $theRenderInfo->activity[48]->itemid;
-              // echo $row[$count]->project-id;
-//echo $value->project-id;
+
     echo $value->description;
               echo      $value->datetime;
               echo $value->activitytype;
-      //        echo $value->project-name;
+
               echo $value->link;
               echo $value->extradescription;
               echo $value->fromusername;
               echo $value->itemid;
-              //echo $theRenderInfo->activity[0]['project-id'];
-              //$theRenderInfo->activity[48]->itemid;
+
 
           }
-   //   }
+
     }
